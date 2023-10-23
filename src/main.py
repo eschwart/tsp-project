aff824ca6ec82bcea63faf5002ea4814efac4350
eschwart <eschwart@mtu.edu>
2023-10-22 20:06:49 -0400

    update

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
+from config import *
 from data import *
-from bot import *
 
 # retrieve bot env token
 token = get_token()
@@ -9,10 +9,20 @@ if token == None:
 # instantiate `Bot`
 bot = init_bot()
 
+# instantiate user data map
+data = Database()
+
 
 @bot.command()
 async def set_height(ctx, arg):
-    pass
+    user = data.get_user(ctx.author.id)
+    user.set_height(arg)
+    await ctx.send("Done!")
+
+@bot.command()
+async def get_height(ctx):
+    user = data.get_user(ctx.author.id)
+    await ctx.send(f"Height of {ctx.author}: {user.height}")
 
 @bot.command()
 async def set_weight(ctx, arg):
@@ -26,11 +36,11 @@ async def add_food(ctx, arg):
 async def add_workout(ctx, *, args):
     pass
 
-
 # debugging purposes
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 
+# run the bot
 bot.run(token)
