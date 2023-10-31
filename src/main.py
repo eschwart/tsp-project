b86086db976f78839f2c52e9a768dcfd409986f2
Evan Schwartzentruber <110786443+eschwart@users.noreply.github.com>
2023-10-31 18:25:31 -0400

    refac, doc, and `before_invoke` impl

--- src/main.py
+++ src/main.py
@@ -3,8 +3,6 @@ from data import *
 
 # retrieve bot env token
 token = get_token()
-if token == None:
-    exit("Missing `TOKEN` variable")
 
 # instantiate `Bot`
 bot = init_bot()
@@ -13,33 +11,67 @@ bot = init_bot()
 data = Database()
 
 
-@bot.command()
-async def set_height(ctx, arg):
-    user = data.get_user(ctx.author.id)
-    user.set_height(arg)
-    await ctx.send("Done!")
+@bot.before_invoke
+async def validate_user(ctx: Context):
+    """Ensure the user is in the database before running any commands"""
+    user_id: int = ctx.author.id  # id of the author of the message
+
+    # instantiate the user if not found
+    if not data.has_user(user_id):
+        data.new_user(user_id)
+
 
 @bot.command()
-async def get_height(ctx):
+async def get_height(ctx: Context):
+    """Send the current height of the user"""
     user = data.get_user(ctx.author.id)
-    await ctx.send(f"Height of {ctx.author}: {user.height}")
+    msg = f"Height of {ctx.author.mention}: `{user.get_height()}`"
+    await ctx.send(msg)
+
+
+@bot.command()
+async def get_weight(ctx: Context):
+    """Send the current weight of the user"""
+    user = data.get_user(ctx.author.id)
+    msg = f"Weight of {ctx.author.mention}: `{user.get_weight()}`"
+    await ctx.send(msg)
+
+
+@bot.command()
+async def set_height(ctx, arg):
+    """Set the current height of the user"""
+    user = data.get_user(ctx.author.id)
+    user.set_height(arg)
+    await ctx.send("Done")
+
 
 @bot.command()
 async def set_weight(ctx, arg):
-    pass
+    """Set the current weight of the user"""
+    user = data.get_user(ctx.author.id)
+    user.set_weight(arg)
+    await ctx.send("Done")
 
+
+# TODO: figure out how we want to structure the food/calorie system
 @bot.command()
-async def add_food(ctx, arg):
+async def add_food(ctx, item, calories):
+    """Add a food item with its number of calories for the user"""
     pass
 
+
+# TODO: implement `User::add_workout``
 @bot.command()
 async def add_workout(ctx, *, args):
+    """Add a workout for the user"""
     pass
 
-# debugging purposes
+
+# Debugging
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def ping(ctx: Context):
+    """Send a reply message with the latency in milliseconds"""
+    await ctx.send(f"pong `{int(ctx.bot.latency * 1000)} ms`")
 
 
 # run the bot
